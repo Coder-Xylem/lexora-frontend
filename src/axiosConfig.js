ea9bbe864f5e9c51697819f1729e220f740ba49f
Coder-philosopher <tentaciouspersona@gmail.com>
2024-11-30 03:56:16 +0530

    test 1.2

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://testb-phi.vercel.app',
+  baseURL: 'https://testb-phi.vercel.app/api',
   withCredentials: true, });
 
 export default axiosInstance;
