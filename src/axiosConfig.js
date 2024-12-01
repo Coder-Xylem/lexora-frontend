a54edf71d1b6717eb6b0f58261d5391dac8c20a8
Coder-philosopher <tentaciouspersona@gmail.com>
2024-12-01 22:38:00 +0530

    test 1.7

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,12 @@ const axiosInstance = axios.create({
   baseURL: ('https://lexora-backend.vercel.app/api/'),
   withCredentials: true, 
   headers: {
-    'Content-Type': 'application/json', // Set necessary headers
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Credentials': true
+    
   }
 });
 
