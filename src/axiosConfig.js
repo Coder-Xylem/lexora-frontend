b00d80794acae65fc1f51da62e3d86b47ead1de4
Coder-philosopher <tentaciouspersona@gmail.com>
2024-12-04 02:23:48 +0530

    testing railway

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://lexora-backend-lbmv.vercel.app/api/',
+  baseURL: 'https://lexora-backend-production.up.railway.app/api/',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
