de42d4c270664928cf038e987cfc1f5899dc27f7
Coder-philosopher <tentaciouspersona@gmail.com>
2024-12-04 02:07:23 +0530

    test 1.17

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -4,9 +4,9 @@ import { emojify } from 'node-emoji';
 import axios from '../axiosConfig';
 import PropTypes from 'prop-types';
 
-const socket = io('https://lexora-backend-lbmv.vercel.app', {
-  path: '/ws', // Custom path matching backend
+const socket = io('wss://lexora-backend-lbmv.vercel.app', {
   transports: ['websocket', 'polling'],
+  withCredentials: true,
 });
 
 
