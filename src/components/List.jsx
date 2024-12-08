22225f7929ef13cc4320da50c23dd53baddf8113
Coder-philosopher <tentaciouspersona@gmail.com>
2024-12-08 23:21:26 +0530

    try 2.5

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -140,9 +140,9 @@ function ContactList() {
                 </div>
               )
             ))}
-                  <div className="text-sm text-gray-300 mt-4 flex items-center space-x-2">
+                  {/* <div className="text-sm text-gray-300 mt-4 flex items-center space-x-2">
             <FaExclamationCircle className="text-yellow-400" />
-            {/* <p>
+            <p>
               Click Here to Sign-Out.(Remember your Lexus ID and Password to Sign-in again for secure conversations) 
               <button
                 onClick={() => {
